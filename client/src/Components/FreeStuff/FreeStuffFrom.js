a8f52d1a419be0f8284a8ef74c98d97f04b8cd8a
Robert Bedenbaugh <robertbedenbaughjr@gmail.com>
2022-03-21 09:26:12 -0400

    hhh

---
+++ client/src/Components/FreeStuff/FreeStuffFrom.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class FreeStuffForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( <div>
+            FreeStuffForm
+        </div> );
+    }
+}
+ 
+export default FreeStuffForm;
---
+++ client/src/Components/FreeStuff/FreeStuffSingle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FreeStuffSingle = () => {
+    return ( <div>
+        FreeStuffSingle
+    </div> );
+}
+ 
+export default FreeStuffSingle;
